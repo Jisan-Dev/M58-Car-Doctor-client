f44b47bb9dae0104ff7e3c82e3a2d8ce225918c2
Jisun696 <istiakkashem35@gmail.com>
2024-05-03 00:54:11 +0600

    login page completed

--- src/Routes/router.jsx
+++ src/Routes/router.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from 'react-router-dom';
 import Root from './Root';
 import Home from '../pages/home';
 import SignUp from '../pages/signup';
+import Login from '../pages/login';
 
 const router = createBrowserRouter([
   {
@@ -15,6 +16,10 @@ const router = createBrowserRouter([
       {
         path: '/signUp',
         element: <SignUp />,
+      },
+      {
+        path: '/login',
+        element: <Login />,
       },
     ],
   },
