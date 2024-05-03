c29d93f93aa327be866828e9420f1fb89097f574
Jisun696 <istiakkashem35@gmail.com>
2024-05-03 17:52:14 +0600

    used loader on the route of ServiceDetails page to fetch data. removed useEffect()

--- src/Routes/router.jsx
+++ src/Routes/router.jsx
@@ -25,6 +25,7 @@ const router = createBrowserRouter([
       {
         path: '/services/:id',
         element: <ServiceDetails />,
+        loader: ({ params }) => fetch(`http://localhost:5000/services/${params.id}`),
       },
     ],
   },
--- src/pages/service-details/index.jsx
+++ src/pages/service-details/index.jsx
@@ -1,20 +1,7 @@
-import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useLoaderData } from 'react-router-dom';
 
 const ServiceDetails = () => {
-  const [service, setService] = useState({});
-
-  const { id } = useParams();
-
-  useEffect(() => {
-    console.log(id);
-    fetch(`http://localhost:5000/services/${id}`)
-      .then((res) => res.json())
-      .then((data) => {
-        setService(data);
-        console.log(data);
-      });
-  }, [id]);
+  const service = useLoaderData();
 
   return (
     <div>
